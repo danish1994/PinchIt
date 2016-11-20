0e67a971cbe4b8d0fe47fd077fbcdfb51e0dbc8a
Danish Kamal <danish8802204230@gmail.com>
2016-11-21 00:42:08 +0530

    Initial

---
+++ app.js
@@ -0,0 +1,37 @@
+var bodyparser = require('body-parser')
+var connection = require('./connection')
+var express = require('express')
+var path = require('path')
+var routes = require('./routes')
+var setup = require('./setup')
+var mkdirp = require('mkdirp')
+var app = express()
+var ip = process.env.OPENSHIFT_NODEJS_IP || 'localhost'
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8880
+
+
+connection.init()
+setup()
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
+
+app.use(bodyparser.json({
+    limit: '100mb'
+}))
+
+app.use(bodyparser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 500000
+}))
+
+routes.configure(app)
+
+app.listen(port, ip, function() {
+    console.log('Server listening on port ' + port);
+})
---
+++ models/category.js
@@ -0,0 +1,45 @@
+var connection = require('../connection')
+
+function category() {
+
+    this.conn = null
+    sequelize = null
+    this.category = null
+
+    this.init = function() {
+        this.conn = connection.getConnection()
+        sequelize = connection.getSequelize()
+        this.category = this.conn.define('category', {
+            categoryid: {
+                type: sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            category: sequelize.STRING(100),
+        }, {
+            freezeTableName: true,
+            timestamps: true
+        })
+        this.category.sync()
+    }
+
+    this.post = function(record, response) {
+        this.category.create({
+            category: record.category,
+        }).then(function() {
+            response.send({
+                status: 0,
+                message: 'Category added Successfully'
+            })
+        }).catch(function(error) {
+            response.send({
+                status: 1,
+                message: 'Category not added'
+            })
+        })
+    }
+
+
+}
+
+module.exports = new category()
