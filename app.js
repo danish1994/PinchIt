82d00cff1ceea0d9f0fa4f78aa93e53ee808cc82
Danish Kamal <danish8802204230@gmail.com>
2017-02-12 17:08:22 +0530

    CORS Enabled

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ setup()
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
