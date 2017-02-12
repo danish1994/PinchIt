5df17fa3b655c7a84430c92244be8571d44b0a62
Danish Kamal <danish8802204230@gmail.com>
2017-02-12 16:24:13 +0530

    Admin Page n Page Updated

--- public/admin/admin-script.js
+++ public/admin/admin-script.js
@@ -5,169 +5,164 @@ var image
 
 
 $(document).ready(function() {
-	$('#loading-modal').modal('show')
-	loginHide()
-	adminHide()
-	admin_token = localStorage.getItem('admin_token')
-	if(admin_token){
-		adminShow()	
-	}else{
-		$('#loading-modal').modal('hide')
-		$('#login').show()
-	}
+    $('#loading-modal').modal('show')
+    loginHide()
+    adminHide()
+    admin_token = localStorage.getItem('admin_token')
+    if (admin_token) {
+        adminShow()
+    } else {
+        $('#loading-modal').modal('hide')
+        $('#login').show()
+    }
 })
 
 
-function loginHide(){
-	$('#login').hide()
+function loginHide() {
+    $('#login').hide()
 }
 
-function adminHide(){
-	$('#admin').hide()
-	$('#logout').hide()
+function adminHide() {
+    $('#admin').hide()
+    $('#logout').hide()
 }
 
-function adminShow(){
-	$.ajax({
-		type: 'GET',
-		url: '/post/unverified/',
-		success: function(res){
-			$('#admin').show()
-			$('#logout').show()
-			$('#loading-modal').modal('hide')
-			loadPost(res)
-		},
-		error: function(err){
-			console.log(err)
-		}
-	})
-}
-
-function loadPost(posts){
-	$.each(posts, function (i, post) {
-		$('#post-container').append(postPrototype(post))
-	})
-	$('#post-container').append('<div class="text-center row"> <h4> No More Posts </h4> </div>')
-}
-
-function postPrototype(post){
-	var res = '<div class="col-sm-12">'
-	+'<div class="row">'
-	+'<div class="col-sm-3">'
-	+'<img class="full-width" src="'+post.image+'"/>'
-	+'</div>'
-	+'<div class="col-sm-9">'
-	+'<div class="row">'
-	+'<div class="col-sm-12">'
-	+'<div class="row">'
-	+'<div class="col-sm-8">'
-	+'<h3>'+post.title+'</h3>'
-	+'</div>'
-	+'<div class="col-sm-2">'
-	+'<button class="btn btn-default full-width" onClick="verifyPost('+post.postid+')">Verify</button>'
-	+'</div>'
-	+'<div class="col-sm-2">'
-	+'<button class="btn btn-danger full-width" onClick="deletePost('+post.postid+')">Delete</button>'
-	+'</div>'
-	+'</div>'
-	+'</div>'
-	+'<div class="col-sm-12">'
-	+'<p>'+post.post+'</p>'
-	+'</div>'
-	+'</div>'
-	+'</div>'
-	+'</div>'
-	+'<hr>'
-	+'</div>'
-
-	return res
-}
-
-function deletePost(id){
-	$('#loading-modal').modal('show')
-	$.ajax({
-		type: 'DELETE',
-		url: '/post/'+id,
-		success: function(res){
-			console.log(res)
-			if(res.status == 0){
-				$('#loading-modal').modal('hide')
-				alert('Post Deleted')
-				location.reload()
-			}else{
-				$('#loading-modal').modal('hide')
-				alert(res.message)
-			}
-		},
-		error: function(err){
-			alert('Something Went Wrong. Please Try Again.')
-			$('#loading-modal').modal('hide')
-		}
+function adminShow() {
+    $.ajax({
+        type: 'GET',
+        url: '/post/getall/',
+        success: function(res) {
+            $('#admin').show()
+            $('#logout').show()
+            $('#loading-modal').modal('hide')
+            loadPost(res)
+        },
+        error: function(err) {
+            console.log(err)
+        }
     })
 }
 
-function verifyPost(id){
-	$('#loading-modal').modal('show')
-	$.ajax({
-		type: 'POST',
-		url: '/post/verify/',
-		data: {
-			token: admin_token,
-			postid: id
-		},
-		success: function(res){
-			console.log(res)
-			if(res.status == 0){
-				$('#loading-modal').modal('hide')
-				alert('Post Verified')
-				location.reload()
-			}else{
-				$('#loading-modal').modal('hide')
-				alert(res.message)
-			}
-		},
-		error: function(err){
-			alert('Something Went Wrong. Please Try Again.')
-			$('#loading-modal').modal('hide')
-		}
+function loadPost(posts) {
+    $.each(posts, function(i, post) {
+        $('#post-container').append(postPrototype(post))
+    })
+    $('#post-container').append('<div class="row"> <div class="text-center col-sm-12"> <h4> No More Posts </h4> </div> </div>')
+}
+
+function postPrototype(post) {
+
+    var res = '<div class="col-sm-4">' +
+        '                                <div class="row post">' +
+        '                                    <div class="col-sm-12 post-image">' +
+        '                                        <img src="' + post.image + '" style="width: 100%">' +
+        '                                    </div>' +
+        '                                    <div class="col-sm-12">' +
+        '                                        <h4>' + post.title + '</h4>' +
+        '                                    </div>' +
+        '                                    <div class="col-sm-12">' +
+        '                                        <p>' + post.post + '</p>' +
+        '                                    </div>' +
+        '                                    <div class="col-sm-12 read-more text-center">' +
+        '                                        <a href="' + post.link + '" target="_blank">Read More</a>' +
+        '                                    </div>' +
+        '                                    <div class="col-sm-12">' +
+        '                                        <button class="btn btn-default full-width" onClick="verifyPost(' + post.postid + ')">Verify</button>' +
+        '                                    </div>' +
+        '                                    <div class="col-sm-12">' +
+        '                                        <button class="btn btn-danger full-width" onClick="deletePost(' + post.postid + ')">Delete</button>' +
+        '                                    </div>' +
+        '                                </div>' +
+        '                            </div>';
+
+    return res
+}
+
+function deletePost(id) {
+    $('#loading-modal').modal('show')
+    $.ajax({
+        type: 'DELETE',
+        url: '/post/' + id,
+        success: function(res) {
+            console.log(res)
+            if (res.status == 0) {
+                $('#loading-modal').modal('hide')
+                alert('Post Deleted')
+                location.reload()
+            } else {
+                $('#loading-modal').modal('hide')
+                alert(res.message)
+            }
+        },
+        error: function(err) {
+            alert('Something Went Wrong. Please Try Again.')
+            $('#loading-modal').modal('hide')
+        }
+    })
+}
+
+function verifyPost(id) {
+    $('#loading-modal').modal('show')
+    $.ajax({
+        type: 'POST',
+        url: '/post/verify/',
+        data: {
+            token: admin_token,
+            postid: id
+        },
+        success: function(res) {
+            console.log(res)
+            if (res.status == 0) {
+                $('#loading-modal').modal('hide')
+                alert('Post Verified')
+                location.reload()
+            } else {
+                $('#loading-modal').modal('hide')
+                alert(res.message)
+            }
+        },
+        error: function(err) {
+            alert('Something Went Wrong. Please Try Again.')
+            $('#loading-modal').modal('hide')
+        }
     })
 }
 
 
-$('#admin-login-form').submit(function(event){
-	event.preventDefault()
-	var query = $(this).serialize()
+$('#admin-login-form').submit(function(event) {
+    event.preventDefault()
+    var query = $(this).serialize()
 
-	$('#loading-modal').modal('show')
+    $('#loading-modal').modal('show')
 
-	$.ajax({
-		type: 'POST',
-		url: '/admin/login/',
-		data: query,
-		success: function(res){
-			console.log(res)
-			if(res.status == 0){
-				admin_token = res.message.token
-				localStorage.setItem('admin_token', admin_token)
-				loginHide()
-				adminShow()
-				$('#loading-modal').modal('hide')
-			}else{
-				$('#loading-modal').modal('hide')
-				alert(res.message)
-			}
-		},
-		error: function(err){
-			alert('Something Went Wrong. Please Try Again.')
-			$('#loading-modal').modal('hide')
-		}
+    $.ajax({
+        type: 'POST',
+        url: '/admin/login/',
+        data: query,
+        success: function(res) {
+            console.log(res)
+            if (res.status == 0) {
+                admin_token = res.message.token
+                localStorage.setItem('admin_token', admin_token)
+                loginHide()
+                adminShow()
+                $('#loading-modal').modal('hide')
+            } else {
+                $('#loading-modal').modal('hide')
+                alert(res.message)
+            }
+        },
+        error: function(err) {
+            alert('Something Went Wrong. Please Try Again.')
+            $('#loading-modal').modal('hide')
+        }
     })
 })
 
 
 
 
-$('#logout').click(function(){
-	localStorage.removeItem('admin_token')
-	location.reload()
+$('#logout').click(function() {
+    localStorage.removeItem('admin_token')
+    location.reload()
 })
