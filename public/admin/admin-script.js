e6a6fcefc7c023a2ca89ecd0b931a1194e92264d
Danish Kamal <danish8802204230@gmail.com>
2017-03-24 01:06:12 +0530

    All Education Post Available to Admin Panel for Repost (with or without notification

--- public/admin/admin-script.js
+++ public/admin/admin-script.js
@@ -29,12 +29,22 @@ function adminHide() {
 function adminShow() {
     $.ajax({
         type: 'GET',
-        url: '/post/unverified/',
+        url: '/post/getAll/?category=1',
         success: function(res) {
-            $('#admin').show()
-            $('#logout').show()
-            $('#loading-modal').modal('hide')
-            loadPost(res)
+            $.ajax({
+                type: 'GET',
+                url: '/post/unverified/',
+                success: function(res) {
+                    $('#admin').show()
+                    $('#logout').show()
+                    $('#loading-modal').modal('hide')
+                    loadPost(res)
+                },
+                error: function(err) {
+                    console.log(err)
+                }
+            })
+            loadPostAllEducation(res)
         },
         error: function(err) {
             console.log(err)
@@ -84,13 +94,24 @@ function loadPost(posts) {
     $('#post-container').append('<div class="row"> <div class="text-center col-sm-12"> <h4> No More Posts </h4> </div> </div>')
 }
 
+function loadPostAllEducation(posts) {
+    $.each(posts, function(i, post) {
+        $('#post-container-all-education').append(postPrototypeAllEducation(post))
+    })
+
+    $('#post-container-all-education').append('<div class="row"> <div class="text-center col-sm-12"> <h4> No More Posts </h4> </div> </div>')
+}
+
 function postPrototype(post) {
-
-
     var res = '<div class="col-sm-4"> <div class="row post"> <div class="col-sm-12 post-image"> <img src="' + post.image + '" style="width: 100%"> </div><div class="col-sm-12"> <h4>' + post.title + '</h4> </div><div class="col-sm-12"> <p>' + post.post + '</p><br/> <br/> <p>Pinch By: ' + post.writer.name + '</p></div><div class="col-sm-12 read-more text-center"> <a href="' + post.link + '" target="_blank">Read More</a> </div><div class="col-sm-12"> <button class="btn btn-success full-width" onClick="verifyPost(' + post.postid + ',true)">Verify with Notification</button> </div><div class="col-sm-12"> <button class="btn btn-warning full-width" onClick="verifyPost(' + post.postid + ', false)">Verify without Notification</button> </div><div class="col-sm-12"> <button class="btn btn-danger full-width" onClick="deletePost(' + post.postid + ')">Delete</button> </div><div class="col-sm-12"> <button class="btn btn-primary full-width" data-toggle="modal" data-target="#myModal' + post.postid + '">Edit</button> </div></div><div id="myModal' + post.postid + '" class="modal fade" role="dialog"> <div class="modal-dialog"> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal">&times;</button> <h4 class="modal-title">Edit Post</h4> </div><div class="modal-body"> <div class="col-sm-10 col-sm-offset-1"> <div class="row"> <form class="form-inline update-post" id="' + post.postid + '"> <br/> <div class="col-sm-12"> <img src="' + post.image + '" style="width: 100%; padding-bottom: 20px;"> </div><div class="col-sm-12"> <div class="row"> <div class="col-sm-2"> <label>Title*</label> </div><div class="col-sm-10"> <input type="text" id="post-title' + post.postid + '" name="title" class="form-control full-width" maxlength="60" required value="' + post.title + '"> </div></div></div><div class="col-sm-12"> <div class="row"> <div class="col-sm-2"> <label>Desc*</label> </div><div class="col-sm-10"> <textarea rows="4" cols="50" id="post-data' + post.postid + '" class="form-control full-width" maxlength="320" required>' + post.post + '</textarea> </div></div></div><div class="col-sm-12"> <div class="row"> <div class="col-sm-2"> <label>Link</label> </div><div class="col-sm-10"> <input type="text" id="post-link' + post.postid + '" name="link" class="form-control full-width" value="' + post.link + '"> </div></div></div><div class="col-sm-12"> <input type="submit" value="Submit" class="btn btn-default full-width"> </div></form> </div></div></div><div class="modal-footer"> </div></div></div></div></div>';
-
     return res
 }
+
+function postPrototypeAllEducation(post) {
+    var res = '<div class="col-sm-4"> <div class="row post"> <div class="col-sm-12 post-image"> <img src="' + post.image + '" style="width: 100%"> </div><div class="col-sm-12"> <h4>' + post.title + '</h4> </div><div class="col-sm-12"> <p>' + post.post + '</p><br/> <br/> <p>Pinch By: ' + post.writer.name + '</p></div><div class="col-sm-12 read-more text-center"> <a href="' + post.link + '" target="_blank">Read More</a> </div><div class="col-sm-12"> <button class="btn btn-success full-width" onClick="verifyPost(' + post.postid + ',true)">Repost with Notification</button> </div><div class="col-sm-12"> <button class="btn btn-warning full-width" onClick="verifyPost(' + post.postid + ', false)">Repost without Notification</button> </div></div></div>';
+    return res
+}
+
 
 function deletePost(id) {
     $('#loading-modal').modal('show')
